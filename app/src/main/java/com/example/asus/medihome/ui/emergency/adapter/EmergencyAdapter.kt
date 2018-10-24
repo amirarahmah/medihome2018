1053d7a26e38b18906b1776645b9095018a1e580
amira <amirarahmah1@gmail.com>
2018-10-25 06:58:01 +0700

    add panggil ambulans

--- app/src/main/java/com/example/asus/medihome/ui/emergency/adapter/EmergencyAdapter.kt
+++ app/src/main/java/com/example/asus/medihome/ui/emergency/adapter/EmergencyAdapter.kt
@@ -1,18 +1,21 @@
 package com.example.asus.medihome.ui.emergency.adapter
 
+import android.app.AlertDialog
+import android.content.Context
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
-import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import com.example.asus.medihome.R
 import com.example.asus.medihome.model.Ambulans
-import com.example.asus.medihome.ui.pesanan.PesananAdapter
-import kotlinx.android.synthetic.main.item_emergency.view.*
+import android.content.Intent
+import android.net.Uri
 
-class EmergencyAdapter(var listHospital : ArrayList<Ambulans>) : RecyclerView.Adapter<EmergencyAdapter.MyViewHolder>() {
+
+class EmergencyAdapter(var listHospital : ArrayList<Ambulans>,var mContext : Context)
+    : RecyclerView.Adapter<EmergencyAdapter.MyViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, p1: Int): MyViewHolder {
         val view = LayoutInflater.from(parent.context)
@@ -30,9 +33,13 @@ class EmergencyAdapter(var listHospital : ArrayList<Ambulans>) : RecyclerView.Ad
         holder.jarakRsTv.text = "${hospital.jarak} Km"
 
         holder.callButton.setOnClickListener {
-
+            val phoneNumber = hospital.phone
+            val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", phoneNumber, null))
+            mContext.startActivity(intent)
         }
     }
+
+
 
     class MyViewHolder(itemView : View) : RecyclerView.ViewHolder(itemView){
         var namaRsTv = itemView.findViewById<TextView>(R.id.nama_rs_tv)
